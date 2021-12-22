34d96dc62e190635fa9bd333ba8770f3aae9fbff
Frank <frank.wu>
2021-12-22 09:10:38 +0800

    MOD: components set

---
+++ src/components/sider-page/SliderPage.tsx
@@ -0,0 +1,38 @@
+
+import React from 'react';
+import Carousel from 'nuka-carousel';
+import './slider-page.scss';
+import { ImageType } from '../../types/ImageType';
+
+export default function SlidePage(props: any) {
+    const { imgs } = props;
+    
+    return (
+        <div className="slider-page--wrapper func--full-height">
+            <Carousel
+                heightMode={'first'}
+                transitionMode="fade"
+                cellAlign="center"
+                defaultControlsConfig = {
+                    {
+                        // hidden the paging-dots by class
+                        pagingDotsContainerClassName: 'paging-dots--container',
+                        prevButtonText: ' ',
+                        prevButtonClassName: 'prev-button iconfont icon-arrow-left-bold',
+                        nextButtonText: ' ',
+                        nextButtonClassName: 'next-button iconfont icon-arrow-right-bold',
+                    }
+                }
+                speed={1500}
+            >
+                {
+                    imgs.map((image: any) => (<img key={image.src} src={image.src} alt="Opps."/>))
+                }
+            </Carousel>
+        </div>
+    )
+}
+
+// SlidePage.propTypes = {
+//   imgs: Array<ImageType>()
+// }
--- src/pages/architecture/Architecture.tsx
+++ src/pages/architecture/Architecture.tsx
@@ -1,61 +1,14 @@
 
-import React, { useState,  useEffect, useRef } from 'react';
-import Carousel from 'nuka-carousel';
-import './architecture.scss';
-import { getArchitectureImgs } from '../../apis/architecture';
-import { ImageType } from '../../types/ImageType';
-import { useInterval } from '../../hooks/interval';
+import React, { useState,  useEffect } from 'react';
+import SlidePage from '../../components/sider-page/SliderPage';
 
-const DURATION: number = 5000;
-const INTIAL_IMG_CURRENT_INDEX =  0;
+import { getArchitectureImgs } from '../../apis/architecture';
+import { useImgs } from '../../hooks/imgs';
 
 export default function Architecture() {
+    const [imgs] = useImgs([], getArchitectureImgs);
 
-    useEffect(() => {
-        getArchitectureImgs().then((res: any ) => {
-            setImgs(res);
-        });
-    }, []);
-
-    const [imgs, setImgs] = useState<Array<ImageType> >([]);
-    const [imgCurrentIndex, setImgCurrentIndex] = useState<number>(INTIAL_IMG_CURRENT_INDEX);
-    useInterval(() => {
-        if (imgCurrentIndex < imgs.length - 1) {
-            setImgCurrentIndex(imgCurrentIndex + 1);
-        } else {
-            setImgCurrentIndex(INTIAL_IMG_CURRENT_INDEX);
-        }
-    },  DURATION);
-    
     return (
-        <div className="architecture--wrapper func--full-height">
-            <Carousel
-                heightMode={'first'}
-                transitionMode="fade"
-                cellAlign="center"
-                defaultControlsConfig = {
-                    {
-                        // hidden the paging-dots by class
-                        pagingDotsContainerClassName: 'paging-dots--container',
-                        prevButtonText: ' ',
-                        prevButtonClassName: 'prev-button iconfont icon-arrow-left-bold',
-                        nextButtonText: ' ',
-                        nextButtonClassName: 'next-button iconfont icon-arrow-right-bold',
-                    }
-                }
-                speed={1500}
-            >
-                {
-                    imgs.map(image => (<img key={image.src} src={image.src} alt="Opps."/>))
-                }
-            </Carousel>
-            {/* {
-                imgs.map((image, index) => {
-                    const classes = ['img'];
-                    if (imgCurrentIndex === index) classes.push('is-show');
-                    return (<img key={image.src} className={classes.join(' ')} src={image.src} alt="Opps."/>)
-                })
-            } */}
-        </div>
+        <SlidePage imgs={imgs}></SlidePage>
     )
 }
